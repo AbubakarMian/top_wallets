0f7dba2669371d19463059cb0140302929b02a71
AbubakarMian <abubakrmianmamoon@gmail.com>
2024-10-10 19:41:28 +0500

    top traders fix

--- top_projects.js
+++ top_projects.js
@@ -4,97 +4,118 @@ const app = express();
 const PORT = 5003;
 
 async function getDataFromPage(url, limit) {
-    const browser = await chromium.launch({
-        headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-blink-features=AutomationControlled',
-            '--disable-dev-shm-usage',
-            '--disable-gpu',
-            '--window-size=1280,720'
-        ]
-    });
 
-    const page = await browser.newPage();
-    await page.setExtraHTTPHeaders({
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-      });
-     await page.goto(url);
-     await page.waitForTimeout(3000);
-    
-    const tableXPath = '//*[@id="root"]/div/main/div/div[2]/div[4]';
-    let retries = 10;
+    try {
+        const browser = await chromium.launch({
+            // headless: false,
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-blink-features=AutomationControlled',
+                '--disable-dev-shm-usage',
+                '--disable-gpu',
+                '--window-size=1280,720'
+            ]
+        });
 
-    for (let attempt = 0; attempt < retries; attempt++) {
-        if (await page.$(tableXPath)) {
-            console.log(`Table found on attempt ${attempt + 1}`);
-            break;
-        } else {
-            console.log(`Table not found, retrying...`);
-            await page.waitForTimeout(1000);
+        const page = await browser.newPage();
+        await page.setExtraHTTPHeaders({
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        });
+        await page.goto(url);
+        await page.waitForTimeout(3000);
+
+        const tableXPath = '//*[@id="root"]/div/main/div/div[2]/div[4]';
+        let retries = 10;
+
+        for (let attempt = 0; attempt < retries; attempt++) {
+            if (await page.$(tableXPath)) {
+                console.log(`Table found on attempt ${attempt + 1}`);
+                break;
+            } else {
+                console.log(`Table not found, retrying...`);
+                await page.waitForTimeout(1000);
+            }
         }
-    }
-    await page.waitForSelector('a.ds-dex-table-row.ds-dex-table-row-top');
+        // await page.waitForSelector('a.ds-dex-table-row.ds-dex-table-row-top');
 
-    const rows = await page.$$('a.ds-dex-table-row.ds-dex-table-row-top');
-    console.log(`Rows found: ${rows.length}`);
+        const rows = await page.$$('a.ds-dex-table-row.ds-dex-table-row-top');
 
-    const data = [];
-    const insertedNames = new Set();
-    for (let i = 0; i < rows.length; i++) {
-        try {
+        // const elements = page.locator('.custom-1egwgdj');
+        // const count = await elements.count();
+        if (rows.length == 0) {
+            return [];
+        }
+
+        console.log(`Rows found: ${rows.length}`);
+
+        const data = [];
+        const insertedNames = new Set();
+        for (let i = 0; i < rows.length; i++) {
             const wallet_address = (await rows[i].getAttribute('href')).split('/').pop();
-            
+            console.log('c 1');
             const all_name = await rows[i].$('div.ds-table-data-cell.ds-dex-table-row-col-token');
             const all_name_spans = await all_name.$$('span');
-            const name = (await all_name_spans[1].innerText()).trim() +' '+
-                         (await all_name_spans[2].innerText()).trim() +' '+
-                         (await all_name_spans[3].innerText()).trim() +' '+
-                         (await all_name_spans[4].innerText()).trim() +' '+
-                         (await all_name_spans[5].innerText()).trim();
-            if (insertedNames.has(name)){
+            const name =
+                ((await all_name_spans[1]?.innerText())?.trim() || "") + ' ' +
+                ((await all_name_spans[2]?.innerText())?.trim() || "") + ' ' +
+                ((await all_name_spans[3]?.innerText())?.trim() || "") + ' ' +
+                ((await all_name_spans[4]?.innerText())?.trim() || "") + ' ' +
+                ((await all_name_spans[5]?.innerText())?.trim() || "");
+            if (insertedNames.has(name)) {
                 continue;
             }
-            const price = await rows[i].$('div.ds-table-data-cell.ds-dex-table-row-col-price');  
-            
-            const all_column_val = await rows[i].$$('div.ds-table-data-cell');     
+            const price = await rows[i].$('div.ds-table-data-cell.ds-dex-table-row-col-price');
+            console.log('c 3');
+
+            const all_column_val = await rows[i].$$('div.ds-table-data-cell');
+            console.log('c 4');
+
             const txns = all_column_val[3];
+            console.log('c 5');
+
             const volumn = all_column_val[4];
+            console.log('c 6');
+
             const liquidity = all_column_val[10];
-            
-                const nameValue = name;
-                const priceValue = (await price.innerText()).trim();
-                const txnsValue = (await txns.innerText()).trim();
-                const volumnValue = (await volumn.innerText()).trim();
-                const liquidityValue = (await liquidity.innerText()).trim();
-                data.push({ wallet_address:wallet_address, name: nameValue,
-                    price:priceValue,
-                    txns:txnsValue,
-                    volumn:volumnValue,
-                    liquidity:liquidityValue,
-                 });
-                 insertedNames.add(nameValue);
+            console.log('c 7');
 
-                if (data.length >= limit) break;
-                
-        } catch (error) {
-            console.error(`Error in row ${i}: ${error.message}`);
+            const nameValue = name;
+            const priceValue = ((await price?.innerText())?.trim() || "");
+            const txnsValue = ((await txns?.innerText())?.trim() || "");
+            const volumnValue = ((await volumn?.innerText())?.trim() || "");
+            const liquidityValue = ((await liquidity?.innerText())?.trim() || "");
+            data.push({
+                wallet_address: wallet_address, name: nameValue,
+                price: priceValue,
+                txns: txnsValue,
+                volumn: volumnValue,
+                liquidity: liquidityValue,
+            });
+            insertedNames.add(nameValue);
+
+            if (data.length >= limit) break;
+
+
         }
-    }
 
-    await browser.close();
-    return data;
+        await browser.close();
+        return data;
+    } catch (error) {
+        console.error(` ${error.message}`);
+        return [];
+    }
 }
 
 app.get('/get_projects', async (req, res) => {
-    
+
 
     // const url = `https://dexscreener.com/solana/raydium?embed=1&theme=dark&info=1&rankBy=volume&order=desc`;
-    
+
     try {
 
-        
+
         let limit = parseInt(req.query.limit) || 5;
         let volume = req.query.volume || "";
         let liquidity = req.query.liquidity || "";
@@ -103,17 +124,17 @@ app.get('/get_projects', async (req, res) => {
         let max_market_cap = req.query.max_market_cap || "";
 
 
-        let queryParams = [ `embed=1`, `theme=dark`, `info=1`];
+        let queryParams = [`embed=1`, `theme=dark`, `info=1`];
 
         if (!['24h', '6h', '1h', '5m'].includes(time)) {
             time = '24h';
             let score = '';
-            score = time=='24h' ? 'H24' : time=='6h'?'H6':'M5';
-            score = 'trendingScore'+score;
-            queryParams.push(`rankBy=${score}`);
-            queryParams.push(`order=desc`);
+            score = time == '24h' ? 'H24' : time == '6h' ? 'H6' : 'M5';
+            score = 'trendingScore' + score;
+            // queryParams.push(`rankBy=${score}`);
+            // queryParams.push(`order=desc`);
         }
-        
+
 
         if (volume) {
             queryParams.push(`min24HVol=${volume}`);
@@ -127,9 +148,9 @@ app.get('/get_projects', async (req, res) => {
         if (max_market_cap) {
             queryParams.push(`maxMarketCap=${max_market_cap}`);
         }
-        const url = `https://dexscreener.com/gainers/${time}?${queryParams.join('&')}`;
+        const url = `https://dexscreener.com/solana/raydium/${time}?${queryParams.join('&')}`;
 
-        console.log('my url',url);
+        console.log('my url', url);
         // const url = `https://dexscreener.com/solana/raydium?embed=1&theme=dark&info=1&rankBy=volume&order=desc`;
         const data = await getDataFromPage(url, limit);
         console.log(`Received request for : with limit: ${limit}`);
